c580053dccae2e32f32039cde764f19bd6fb9ac6
sravankumar0523 <116141924+sravankumar0523@users.noreply.github.com>
2022-10-29 23:34:37 +0530

    Create shell-script.sh

---
+++ shell-script.sh
@@ -0,0 +1,14 @@
+echo 1. Directory
+ls -a
+echo
+echo 2. Is Java Installed
+java -version
+echo
+echo 3. Is Git Installed
+git --version
+echo
+echo 4. script run by?
+whoami
+echo
+echo 5. How is the Disc laid out?
+df
